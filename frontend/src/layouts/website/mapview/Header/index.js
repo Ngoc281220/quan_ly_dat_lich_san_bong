698084c51c9e7831068a1814b85d26af4ec178f8
dev_ka_ka <ngocnv@sankei-bc.co.jp>
2025-03-06 23:36:37 +0700

    update code map

--- frontend/src/layouts/website/mapview/Header/index.js
+++ frontend/src/layouts/website/mapview/Header/index.js
@@ -1,37 +1,36 @@
-import { Container, Navbar, Form, FormControl, Nav, Button } from "react-bootstrap";
-import { FaSearch, FaHome, FaMap, FaStar, FaUser } from "react-icons/fa";
+import { Container, Navbar, Form, FormControl, Button } from "react-bootstrap";
+import { FaSearch } from "react-icons/fa";
+
 function HeaderMap() {
   return (
-    <>
-      <Navbar bg="light" expand="lg" className="p-3">
-        <Container>
-          <Form className="d-flex flex-grow-1">
-            <FormControl
-              type="search"
-              placeholder="Tìm kiếm sân quanh đây"
-              className="me-2"
-            />
-            <Button variant="outline-success">
-              <FaSearch />
+    <Navbar expand="lg" className="header-navbar">
+      <Container  fluid className="header-container">
+        {/* Ô tìm kiếm */}
+        <Form className="search-form">
+          <FormControl type="search" placeholder="Tìm kiếm sân quanh đây" className="search-input" />
+        </Form>
+
+        {/* Các button filter */}
+        <div className="filter-buttons">
+          {[
+            "Sân pickleball",
+            "Sân cầu lông",
+            "Sân bóng đá",
+            "Sân tennis",
+            "Sân bóng chuyền",
+            "Sân bóng rổ",
+            "Sân phức hợp",
+            "Sân golf",
+          ].map((item, index) => (
+            <Button key={index} variant="outline-primary" className="filter-button">
+              {item}
             </Button>
-          </Form>
-        </Container>
-      </Navbar>
-      <Container className="filter-container">
-        {[
-          "Sân pickleball",
-          "Sân cầu lông",
-          "Sân bóng đá",
-          "Sân tennis",
-          "Sân bóng chuyền",
-        ].map((item, index) => (
-          <Button key={index} variant="outline-primary" className="m-1">
-            {item}
-          </Button>
-        ))}
+          ))}
+        </div>
       </Container>
-    </>
+    </Navbar>
   );
 }
 
 export default HeaderMap;
+
